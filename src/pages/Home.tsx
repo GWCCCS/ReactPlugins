8b926782f4aaef39361a53dfcbefda7e90d124d8
jonathanbraverman <109186475+jonathanbraverman@users.noreply.github.com>
2022-11-16 10:02:52 -0500

    Menu integrated

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { IonBadge, IonButton, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCheckbox, IonContent, IonFab, IonFabButton, IonHeader, IonIcon, IonItem, IonLabel, IonList, IonNote, IonPage, IonThumbnail, IonTitle, IonToolbar } from '@ionic/react';
+import { IonBadge, IonButton, IonButtons, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCheckbox, IonContent, IonFab, IonFabButton, IonHeader, IonIcon, IonItem, IonLabel, IonList, IonMenu, IonMenuButton, IonNote, IonPage, IonThumbnail, IonTitle, IonToolbar } from '@ionic/react';
 import { RouteComponentProps } from 'react-router';
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
@@ -8,55 +8,68 @@ import { add } from 'ionicons/icons';
 const Home: React.FC<RouteComponentProps> = (props) => {
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>CSC 262 App Example</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonList>
-          <IonItem>
-            <IonCheckbox slot="start"/>
-            <IonLabel>
-              <h1>Create Idea</h1>
-              <IonNote>Run Idea by Avery</IonNote>
-              <IonBadge color="success" slot="end">10 Days</IonBadge>
-            </IonLabel>
-          </IonItem>
-        </IonList>
-        <IonCard color = "warning">
-          <IonCardHeader>
-            <IonCardTitle>CSC 262 App Example 3</IonCardTitle>
-            <IonCardSubtitle>Card Subtitle</IonCardSubtitle>
-          </IonCardHeader>
-
-          <IonCardContent>
-            The quick brown fox...
-          </IonCardContent>
+      <IonMenu contentId="main-content">
+        <IonHeader>
+          <IonToolbar>
+            <IonTitle>Menu Content</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <IonContent className="ion-padding">This is the menu content.</IonContent>
+      </IonMenu>
+      <IonPage id="main-content">
+        <IonHeader>
+          <IonToolbar>
+            <IonButtons slot="start">
+              <IonMenuButton></IonMenuButton>
+            </IonButtons>
+            <IonTitle>CSC 262 App Example</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <IonContent fullscreen>
           <IonList>
-          <IonItem>
-            <IonThumbnail slot="start">
-              <img alt="Silhouette of mountains" src="https://ionicframework.com/docs/img/demos/thumbnail.svg" />
-            </IonThumbnail>
-            <IonLabel>Item</IonLabel>
-          </IonItem>
+            <IonItem>
+              <IonCheckbox slot="start"/>
+              <IonLabel>
+                <h1>Create Idea</h1>
+                <IonNote>Run Idea by Avery</IonNote>
+                <IonBadge color="success" slot="end">10 Days</IonBadge>
+              </IonLabel>
+            </IonItem>
           </IonList>
-          <IonButton fill="clear" routerLink="/haptic">Haptic</IonButton>
-          <IonButton fill="clear" routerLink="/device">Device</IonButton>
-          <IonButton fill="clear" routerLink="/statusBar">Status</IonButton>
-          <IonButton fill="clear" routerLink="/motion">Motion</IonButton>          
-          <IonButton fill="clear" routerLink="/cct">classcompTemplate</IonButton>     
-          <IonButton fill="clear" routerLink="/fcptv">funccompViewTemplate</IonButton> 
-          <IonButton fill="clear" routerLink="/geo">Geo</IonButton> 
-          <IonButton fill="clear" routerLink="/key">keyboard</IonButton>  
-        </IonCard>
-        <IonFab vertical='bottom' horizontal='end' slot='fixed'>
-          <IonFabButton onClick={() => props.history.push('/new2')}>
-            <IonIcon icon={add}/>
-          </IonFabButton>
-        </IonFab>
-        <ExploreContainer/>
-      </IonContent>
+          <IonCard color = "warning">
+            <IonCardHeader>
+              <IonCardTitle>CSC 262 App Example 3</IonCardTitle>
+              <IonCardSubtitle>Card Subtitle</IonCardSubtitle>
+            </IonCardHeader>
+
+            <IonCardContent>
+              The quick brown fox...
+            </IonCardContent>
+            <IonList>
+            <IonItem>
+              <IonThumbnail slot="start">
+                <img alt="Silhouette of mountains" src="https://ionicframework.com/docs/img/demos/thumbnail.svg" />
+              </IonThumbnail>
+              <IonLabel>Item</IonLabel>
+            </IonItem>
+            </IonList>
+            <IonButton fill="clear" routerLink="/haptic">Haptic</IonButton>
+            <IonButton fill="clear" routerLink="/device">Device</IonButton>
+            <IonButton fill="clear" routerLink="/statusBar">Status</IonButton>
+            <IonButton fill="clear" routerLink="/motion">Motion</IonButton>          
+            <IonButton fill="clear" routerLink="/cct">classcompTemplate</IonButton>     
+            <IonButton fill="clear" routerLink="/fcptv">funccompViewTemplate</IonButton> 
+            <IonButton fill="clear" routerLink="/geo">Geo</IonButton> 
+            <IonButton fill="clear" routerLink="/key">keyboard</IonButton>  
+          </IonCard>
+          <IonFab vertical='bottom' horizontal='end' slot='fixed'>
+            <IonFabButton onClick={() => props.history.push('/new2')}>
+              <IonIcon icon={add}/>
+            </IonFabButton>
+          </IonFab>
+          <ExploreContainer/>
+        </IonContent>
+      </IonPage>
     </IonPage>
   );
 };
