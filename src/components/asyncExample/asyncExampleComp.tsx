3d69deff54a6859044770407dd210f886326ec7e
jonathanbraverman <109186475+jonathanbraverman@users.noreply.github.com>
2022-11-21 11:36:57 -0500

    Better description on webpage

--- src/components/asyncExample/asyncExampleComp.tsx
+++ src/components/asyncExample/asyncExampleComp.tsx
@@ -26,7 +26,7 @@ export class asyncExampleComp extends Component {
         let current_time = new Date()
         const sec1 = current_time.getSeconds()
         console.log('await calling @', current_time.getSeconds());
-        const result = await this.resolveAfter2Seconds();
+        const result = await this.resolveAfter2Seconds();    
         console.log('await result:' + result);
         current_time = new Date()
         const sec = current_time.getSeconds()
@@ -59,6 +59,9 @@ export class asyncExampleComp extends Component {
         current_time = new Date()
         const sec = current_time.getSeconds()
         console.log("\tCall w/CB end @", sec)
+        this.setState({
+            property3: JSON.stringify("Entry Time: " + sec1 + "--- Exit Time: " + sec)
+        });        
       }
 
       reportCallback = (value : any, sec1: any) => {
@@ -67,7 +70,7 @@ export class asyncExampleComp extends Component {
         const sec = current_time.getSeconds()
         console.log("\tCallBack @", sec)
         this.setState({
-            property3: JSON.stringify("Entry Time: " + sec1 + "--- Exit Time: " + sec + "  " + value)
+            property3: this.state.property3  + JSON.stringify("  Call Back @  " + sec + "  " + value)
         });
       }
 
